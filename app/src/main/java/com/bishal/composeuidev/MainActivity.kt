345a87c3c7a27eb80ccfa0a0632427ea8b9eea19
bishalprasad321 <bishalprasad321@gmail.com>
2023-08-26 23:30:51 +0530

    Updated UI for landscape and portrait mode

--- app/src/main/java/com/bishal/composeuidev/MainActivity.kt
+++ app/src/main/java/com/bishal/composeuidev/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bishal.composeuidev
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,7 +11,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -23,16 +25,28 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,23 +59,179 @@ import androidx.compose.ui.unit.dp
 import com.bishal.composeuidev.ui.theme.ComposeUIDevTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeUIDevTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                val windowSize = calculateWindowSizeClass(activity = this)
+                MyApp(windowSize)
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun MyApp(windowSize: WindowSizeClass) {
+    when(windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            MyAppPortrait()
+        }
+        WindowWidthSizeClass.Expanded -> {
+            MyAppLandscape()
+        }
+    }
+}
+
+@Composable
+fun MyAppPortrait() {
+    ComposeUIDevTheme {
+        Scaffold(
+            bottomBar = { BottomNavigation() }
+        ) {
+                paddingValues -> HomeScreen(Modifier.padding(paddingValues))
+        }
+    }
+}
+
+@Composable
+fun MyAppLandscape() {
+    ComposeUIDevTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Row {
+                SideNavigationRail()
+                HomeScreen()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyAppLandscapePreview() {
+    MyAppLandscape()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyAppPortraitPreview() {
+    MyAppPortrait()
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun MyAppPortraitDarkPreview() {
+    MyAppPortrait()
+}
+
+@Composable
+fun BottomNavigation(
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        modifier = modifier,
+        containerColor = MaterialTheme.colorScheme.surfaceVariant
+    ) {
+        NavigationBarItem(
+            selected = true,
+            onClick = { },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(
+                        id = R.string.bottom_navigation_home
+                    )
+                )
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(
+                        id = R.string.bottom_navigation_profile
+                    )
+                )
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BottomNavigationPreview() {
+    ComposeUIDevTheme {
+        BottomNavigation()
+    }
+}
+
+@Composable
+fun SideNavigationRail(
+    modifier: Modifier = Modifier
+) {
+   NavigationRail(
+       modifier = modifier.padding(start = 8.dp, end = 8.dp),
+       containerColor = MaterialTheme.colorScheme.background
+   ) {
+       Column(
+           modifier = Modifier.fillMaxHeight(),
+           verticalArrangement = Arrangement.Center,
+           horizontalAlignment = Alignment.CenterHorizontally
+       ) {
+           NavigationRailItem(
+               selected = true,
+               onClick = { },
+               icon = {
+                   Icon(
+                       imageVector = Icons.Default.Home,
+                       contentDescription = null
+                   )
+               },
+               label = {
+                   Text(
+                       text = stringResource(id = R.string.bottom_navigation_home)
+                   )
+               }
+           )
+           Spacer(modifier = Modifier.height(8.dp))
+           NavigationRailItem(
+               selected = false,
+               onClick = { },
+               icon = {
+                   Icon(
+                       imageVector = Icons.Default.AccountCircle,
+                       contentDescription = null
+                   )
+               },
+               label = {
+                   Text(
+                       text = stringResource(id = R.string.bottom_navigation_profile)
+                   )
+               }
+           )
+       }
+   }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SideNavigationRailPreview() {
+    SideNavigationRail()
+}
+
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier
@@ -75,8 +245,10 @@ fun SearchBar(
                 contentDescription = null
             )
         },
-        colors = TextFieldDefaults.textFieldColors(
-            containerColor = MaterialTheme.colorScheme.surface
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surface,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            disabledContainerColor = MaterialTheme.colorScheme.surface,
         ),
         modifier = modifier
             .fillMaxWidth()
@@ -89,10 +261,60 @@ fun SearchBar(
     )
 }
 
-@Preview
 @Composable
-fun SearchBarPreview() {
-    SearchBar()
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        SearchBar(modifier = Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionGrid()
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun HomeScreenPreview() {
+    ComposeUIDevTheme {
+        HomeScreen()
+    }
+}
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        Text(
+            text = stringResource(id = title),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun HomeSectionPreview() {
+    ComposeUIDevTheme {
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+    }
 }
 
 @Composable
@@ -134,14 +356,6 @@ fun AlignYourBodyElement(
                 .paddingFromBaseline(top = 24.dp, bottom = 8.dp),
             style = MaterialTheme.typography.bodyMedium
         )
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
-@Composable
-fun AlignYourBodyRowPreview() {
-    ComposeUIDevTheme {
-        AlignYourBodyRow()
     }
 }
 
@@ -198,11 +412,6 @@ fun FavoriteCollectionCard(
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
-@Composable
-fun FavoriteCollectionGridPreview() {
-    FavoriteCollectionGrid()
-}
 
 private val alignYourBodyData = listOf(
     R.drawable.ab1_inversions to R.string.ab1_inversions,
